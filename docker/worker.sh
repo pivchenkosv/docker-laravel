6cfb5c1bfe05e08432ae8dd69dfe1f83eec785ec
Stanislau Piuchanka <Vladimir@sweetdefeat.com>
2019-08-15 13:13:02 +0300

    add worker process to heroku

--- docker/worker.sh
+++ docker/worker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-php artisan queue:work --verbose --tries=3 --timeout=90
+php ~/artisan queue:work --verbose --tries=3 --timeout=90 &
 
 while [ true ]
     do
-      php artisan schedule:run --verbose --no-interaction &
+      php ~/artisan schedule:run --verbose --no-interaction &
       sleep 60
     done
