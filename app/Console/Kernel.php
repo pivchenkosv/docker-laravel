ae23125c2a57c19cffedebf92744e460d7b07e0b
Stanislau Piuchanka <Vladimir@sweetdefeat.com>
2019-08-15 15:18:31 +0300

    fix job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
 //         $schedule->command('inspire')
 //                  ->hourly();
-        $schedule->job(ExampleJob::class)->everyMinute();
+        $schedule->job(new ExampleJob())->everyMinute();
     }
 
     /**
